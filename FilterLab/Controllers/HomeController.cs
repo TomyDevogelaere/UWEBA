de6b43b272b0d6f92ccf321f4769b301694c03e3
Tomy Devogelaere <tomy.devogelaere@telenet.be>
2022-09-06 13:56:39 +0200

    Adding Filter Lab
    New project for the Filters Lab

---
+++ FilterLab/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using FilterLab.Filters;
+using FilterLab.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace FilterLab.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+       
+        public async Task<IActionResult> Index()
+        {
+            await Task.Delay(1000);
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ FilterLab/Filters/ProfileAttribute.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System.Diagnostics;
+
+namespace FilterLab.Filters;
+
+public class ProfileAttribute: ActionFilterAttribute
+{
+    private Stopwatch? actionTimer;
+    private Stopwatch? resultTimer;
+
+    //public override void OnActionExecuting(ActionExecutingContext context)
+    //{
+    //    actionTimer = Stopwatch.StartNew();
+    //}
+
+    //public override void OnActionExecuted(ActionExecutedContext context)
+    ////{
+    ////    actionTimer?.Stop();
+    ////    if (context.Exception is null && !context.Canceled)
+    ////    {
+    ////        Debug.WriteLine($"Action {context.ActionDescriptor.DisplayName} " +
+    ////            $"elapsed time = {actionTimer?.Elapsed.TotalMilliseconds} ms");
+    ////    }
+    ////}
+    ////public override void OnResultExecuting(ResultExecutingContext context)
+    ////{
+    ////    resultTimer = Stopwatch.StartNew();
+    ////}
+
+    //public override void OnResultExecuted(ResultExecutedContext context)
+    //{
+    //    resultTimer?.Stop();
+    //    if (context.Exception is null && !context.Canceled)
+    //    {
+    //        Debug.WriteLine($"Result {context.ActionDescriptor.DisplayName} " +
+    //            $"method elapsed time = {resultTimer?.Elapsed.TotalMilliseconds} ms");
+    //    }
+    //}
+    public override async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+    {
+       actionTimer = Stopwatch.StartNew();
+        var actionExecutedContext = await next();
+        actionTimer?.Stop();
+        if (actionExecutedContext.Exception == null && !actionExecutedContext.Canceled)
+        {
+          Debug.WriteLine($"Action {context.ActionDescriptor.DisplayName} elapsed time: {actionTimer?.Elapsed.TotalMilliseconds}");
+        }
+    }
+    public override async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+    {
+         resultTimer = Stopwatch.StartNew();
+        var actionExecutedContext = await next();
+        resultTimer?.Stop();
+        if (actionExecutedContext.Exception == null && !actionExecutedContext.Canceled)
+        {
+            Debug.WriteLine($"Action {context.ActionDescriptor.DisplayName} elapsed time: {actionTimer?.Elapsed.TotalMilliseconds}");
+        }
+    }
+}
